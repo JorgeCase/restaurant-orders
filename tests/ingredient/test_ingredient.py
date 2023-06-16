dacd789153434232ddbd24c075d17ed25e7b33d8
JorgeKz <jorgekzbra@gmail.com>
2023-06-16 19:47:50 -0300

    Req 01 - Implementa testes para classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,25 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("farinha")
+    ingredient2 = Ingredient("ovo")
+    ingredient3 = Ingredient("farinha")
+
+    # Test __eq__
+    assert ingredient1 == ingredient3
+    assert not ingredient1 == ingredient2
+
+    # Test __hash__
+    assert hash(ingredient1) == hash(ingredient3)
+    assert hash(ingredient1) != hash(ingredient2)
+
+    # Test __repr__
+    assert str(ingredient1) == "Ingredient('farinha')"
+    assert ingredient1.name == "farinha"
+
+    # Test restrictions
+    assert ingredient1.restrictions == {
+        Restriction.GLUTEN
+    }
